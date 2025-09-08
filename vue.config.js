f269877fba1a1dab101516569ccac84a68391aad
Andrey Zakharov <azakharov@flippingbook.com>
2025-09-08 10:12:09 +0300

    Deployment path fix.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const {defineConfig} = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/alias/',
+  publicPath: '/alias-pwa/',
   pluginOptions: {
     i18n: {
       locale: 'en',
